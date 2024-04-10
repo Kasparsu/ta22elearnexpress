5fa00e705ea90b3023626dec84f1b094a699b677
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-04-10 11:44:41 +0300

    Add templating

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import nunjucks from 'nunjucks';
 import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
@@ -6,19 +7,25 @@ const __dirname = import.meta.dirname;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
 // req => request , res => response
 app.get('/', (req, res) => {
     console.log(req.query);
-    res.sendFile(__dirname + '/views/index.html');
+    res.render('index.njk');
 });
 
 app.get('/answer', (req, res) => {
     console.log(req.query);
-    res.send(req.query);
+    res.render('answer.njk', req.query);
+    
 });
 app.post('/answer', (req, res) => {
     console.log(req.body);
-    res.send(req.body);
+    res.render('answer.njk', {...req.body,...req.query});
 });
 
 app.listen(port, () => {
